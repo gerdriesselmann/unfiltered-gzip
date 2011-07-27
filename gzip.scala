f65203c75a595cec90dfb5367fb94f2dbb1d8022
Gerd Riesselmann <gerd@gerd-riesselmann.net>
2011-07-27 23:27:21 +0200

    Cosmetics, Typos

--- gzip.scala
+++ gzip.scala
@@ -1,9 +1,8 @@
 package net.gerdriesselmann.unfiltered
 
 import unfiltered.response._
-import unfiltered.filter.Plan
 import java.io.PrintWriter
-import unfiltered.request.{Accepts, HttpRequest, AcceptEncoding}
+import unfiltered.request.{HttpRequest, AcceptEncoding}
 import unfiltered.Cycle
 
 
@@ -17,19 +16,19 @@ import unfiltered.Cycle
  * }
  */
 object AcceptsEncoding {
-	trait AcceptingEncoding {
-		val encodingType: String
+    trait AcceptingEncoding {
+        val encodingType: String
 
-		def unapply[T](r: HttpRequest[T]) = r match {
-			case AcceptEncoding(values) =>
-				if (values.exists {s => s.equalsIgnoreCase(encodingType) }) Some(r)
-				else None
-			}
-	}
+        def unapply[T](r: HttpRequest[T]) = r match {
+            case AcceptEncoding(values) =>
+                if (values.exists {s => s.equalsIgnoreCase(encodingType) }) Some(r)
+                else None
+        }
+    }
 
-	object GZip extends AcceptsEncoding.AcceptingEncoding {
-		val encodingType = "gzip"
-	}
+    object GZip extends AcceptsEncoding.AcceptingEncoding {
+        val encodingType = "gzip"
+    }
 }
 
 /**
@@ -38,59 +37,57 @@ object AcceptsEncoding {
  * Taken from here: https://github.com/gerdriesselmann/unfiltered-gzip
  */
 case class GZipResponseString[T](request: HttpRequest[T], content: String) extends Responder[Any] {
-	/**
-	 * Respond
-	 */
-	override def respond(res: HttpResponse[Any]) {
-		request match {
-			case AcceptsEncoding.GZip(_) => streamGZip(res)
-			case _ => streamRes(res)
-		}
-	}
+    /**
+     * Respond
+     */
+    override def respond(res: HttpResponse[Any]) = request match {
+		case AcceptsEncoding.GZip(_) => streamGZip(res)
+		case _ => streamRes(res)
+    }
 
-	/**
-	 * Turn string into bytes
-	 */
-	def contentToBytes = content.getBytes("UTF-8")
+    /**
+     * Turn string into bytes
+     */
+    def contentToBytes = content.getBytes("UTF-8")
 
-	/**
-	 * Stream using the default output stream
-	 */
-	def streamRes(res: HttpResponse[Any]) = stream(res.getOutputStream, content)
+    /**
+     * Stream using the default output stream
+     */
+    def streamRes(res: HttpResponse[Any]) = stream(res.getOutputStream, content)
 
-	/**
-	 * Stream using gzip encoding
-	 */
-	def streamGZip(res: HttpResponse[Any]) = {
-		res.addHeader("Content-Encoding", "gzip")
-		val gos = new java.util.zip.GZIPOutputStream(res.getOutputStream)
-		stream(gos, content)
-	}
+    /**
+     * Stream using gzip encoding
+     */
+    def streamGZip(res: HttpResponse[Any]) = {
+        res.addHeader("Content-Encoding", "gzip")
+        val gos = new java.util.zip.GZIPOutputStream(res.getOutputStream)
+        stream(gos, content)
+    }
 
-	/**
-	 * Write into given stream
-	 */
-	def stream(os: java.io.OutputStream, content: String) {
-		try { os.write(contentToBytes) }
-    	finally { os.close() }
-	}
+    /**
+     * Write into given stream
+     */
+    def stream(os: java.io.OutputStream, content: String) {
+        try { os.write(contentToBytes) }
+        finally { os.close() }
+    }
 }
 
 /**
  * HttpResponse that gzips its content
  */
 class GZipHttpResponse[T](val source: HttpResponse[T]) extends HttpResponse[T](source.underlying) {
-	source.addHeader("Content-Encoding", "gzip")
-	val gzipstream = new java.util.zip.GZIPOutputStream(source.getOutputStream())
-	val writer = new PrintWriter(gzipstream)
+    source.addHeader("Content-Encoding", "gzip")
+    val gzipstream = new java.util.zip.GZIPOutputStream(source.getOutputStream())
+    val writer = new PrintWriter(gzipstream)
 
-	def setContentType(contentType : scala.Predef.String) : scala.Unit = source.setContentType(contentType)
-	def setStatus(statusCode : scala.Int) : scala.Unit = source.setStatus(statusCode)
-	def getWriter() : java.io.PrintWriter = writer
-	def getOutputStream() : java.io.OutputStream = gzipstream
-	def sendRedirect(url : scala.Predef.String) : scala.Unit = source.sendRedirect(url)
-	def addHeader(name : scala.Predef.String, value : scala.Predef.String) : scala.Unit = source.addHeader(name, value)
-	def cookies(cookie : scala.Seq[unfiltered.Cookie]) : scala.Unit	 = source.cookies(cookie)
+    def setContentType(contentType : scala.Predef.String) : scala.Unit = source.setContentType(contentType)
+    def setStatus(statusCode : scala.Int) : scala.Unit = source.setStatus(statusCode)
+    def getWriter() : java.io.PrintWriter = writer
+    def getOutputStream() : java.io.OutputStream = gzipstream
+    def sendRedirect(url : scala.Predef.String) : scala.Unit = source.sendRedirect(url)
+    def addHeader(name : scala.Predef.String, value : scala.Predef.String) : scala.Unit = source.addHeader(name, value)
+    def cookies(cookie : scala.Seq[unfiltered.Cookie]) : scala.Unit     = source.cookies(cookie)
 }
 
 /**
@@ -101,30 +98,26 @@ class GZipHttpResponse[T](val source: HttpResponse[T]) extends HttpResponse[T](s
  * }
  */
 object GZipIntent {
-	/**
-	 * Wrapper around ResponseFunction that switches HttpResponse to GZip, if allowed
-	 */
-	case class GZipResponseFunctionWrapper[A](req: HttpRequest[A], f: ResponseFunction[Any]) extends ResponseFunction[Any] {
-		def apply[T](res: HttpResponse[T]) = req match {
-			case AcceptsEncoding.GZip(_) => {
-				val gos = new GZipHttpResponse(res)
-				f(gos)
-			}
-			case _ => f(res)
-		}
-	}
+    /**
+     * Wrapper around ResponseFunction that switches HttpResponse to GZip, if allowed
+     */
+    case class GZipResponseFunctionWrapper[A](req: HttpRequest[A], f: ResponseFunction[Any]) extends ResponseFunction[Any] {
+        def apply[T](res: HttpResponse[T]) = req match {
+            case AcceptsEncoding.GZip(_) => f(new GZipHttpResponse(res))
+            case _ => f(res)
+        }
+    }
 
-	/**
-	 * Factory to wrap original intent
-	 */
-	def apply[A](inner: Cycle.Intent[A,Any]): Cycle.Intent[A,Any] = {
-		case req @ _ => {
-			inner.orElse({ case _ => Pass }: Cycle.Intent[A,Any])(req) match {
-				case Pass => Pass
-				case responseFunction => GZipResponseFunctionWrapper(req, responseFunction)
-			}
-		}
+    /**
+     * Factory to wrap original intent
+     */
+    def apply[A](inner: Cycle.Intent[A,Any]): Cycle.Intent[A,Any] = {
+        case req @ _ => {
+            inner.orElse({ case _ => Pass }: Cycle.Intent[A,Any])(req) match {
+                case Pass => Pass
+                case responseFunction => GZipResponseFunctionWrapper(req, responseFunction)
+            }
+        }
 
-	}
-	
+    }    
 }
